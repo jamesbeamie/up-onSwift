20f07eef9c325255d976fe99131201ae18ed0ed7
James <admin@James.local>
2019-11-12 16:33:21 +0300

    initial commit

---
+++ Xcode-levelup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode-levelup
+//
+//  Created by James on 11/12/19.
+//  Copyright © 2019 James. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hey!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
