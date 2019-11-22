15c4c82f7082165ce5f914b9d9a3f86c3c18ac72
James <admin@James.local>
2019-11-22 12:35:32 +0300

    Syntax

---
+++ swift MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+// variables annd constants
+var intro:String = "Up on swift"
+// var intro:String = 2 Cannot convert value of type 'Int' to specified type 'String'
+print(intro)
+intro = "new intro"
+print(intro)
+
+let yangu = "personal"
+print(yangu)
+
+// Data types
+var i = 0.2
+
+// i = "james" Cannot assign value of type 'String' to type 'Double'
+print(i)
